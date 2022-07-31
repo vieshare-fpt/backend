1493939b6cb122fffea17109b6d5b014f53f1ae8
huy-raw <ruacon130@gmail.com>
2022-08-01 02:00:50 +0700

    feat: api for transactions

---
+++ src/api/transaction.controller.ts
@@ -0,0 +1,45 @@
+import { HttpPagingResponse } from "@common/http-paging.response";
+import { HttpResponse } from "@common/http.response";
+import { User } from "@common/user";
+import { Sort } from "@constant/sort.enum";
+import { TransactionEnum } from "@constant/type-transaction.enum";
+
+import { TransactionEntity } from "@data/entity/transaction.entity";
+import { PagingRequest } from "@data/request/paging.request";
+import { CurrentUser } from "@decorator/current-user.decorator";
+import { Public } from "@decorator/public.decorator";
+import { Controller, Get, HttpCode, HttpStatus, Query } from "@nestjs/common";
+import { ApiBearerAuth, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
+import { TransactionService } from "@service/transaction/transaction.service";
+
+
+@ApiTags('Transaction')
+@Controller('api/transaction')
+export class TransactionController {
+    constructor(
+        private transactionService: TransactionService) { }
+
+
+
+    @Get('')
+    @ApiBearerAuth()
+    @HttpCode(HttpStatus.OK)
+    @ApiQuery({ name: 'sort', type: 'enum', enum: Sort, example: Sort.DESC, required: false })
+    @ApiQuery({ name: 'bank_id', type: 'string', example: '0258541a-ff5b-11ec-9f25-0242ac130002', required: false })
+    @ApiQuery({ name: 'type', type: 'enum', enum: TransactionEnum, example: TransactionEnum.DEPOSIT, required: false })
+    @ApiQuery({ name: 'isSuccess', type: 'boolean', example: true, required: false })
+    @ApiQuery({ name: 'per_page', type: 'number', example: 10, required: false })
+    @ApiQuery({ name: 'page', type: 'number', example: 1, required: false })
+    async getTransactions(
+        @CurrentUser() user: User,
+        @Query('sort') sort: Sort,
+        @Query("bank_id") bank_id: string,
+        @Query("type") type: TransactionEnum,
+        @Query("isSuccess") isSuccess: boolean,
+        @Query() paging: PagingRequest,
+    ): Promise<HttpResponse<TransactionEntity[]> | HttpPagingResponse<TransactionEntity[]> | any> {
+        const transactions = await
+            this.transactionService.getTransactions(user.id, bank_id, type, isSuccess, sort, paging);
+        return transactions;
+    }
+}
--- src/repository/transaction.repository.ts
+++ src/repository/transaction.repository.ts
@@ -1,16 +1,17 @@
+import { Sort } from "@constant/sort.enum";
+import { BankEntity } from "@data/entity/bank.entity";
 import { TransactionEntity } from "@data/entity/transaction.entity";
 import { UpdateWalletRequest } from "@data/request/update-wallet.request";
-import { EntityRepository, Repository } from "typeorm";
+import { EntityRepository, FindConditions, Repository } from "typeorm";
 
 @EntityRepository(TransactionEntity)
 export class TransactionRepository extends Repository<TransactionEntity> {
     async createTransaction(
         walletId: string,
-        isSuccess: boolean, 
+        isSuccess: boolean,
         updateWallet: UpdateWalletRequest,
     ): Promise<any> {
 
-        
         const transaction = new TransactionEntity();
 
         transaction.date = new Date();
@@ -19,7 +20,33 @@ export class TransactionRepository extends Repository<TransactionEntity> {
         transaction.type = updateWallet.type;
         transaction.isSuccess = isSuccess;
         transaction.walletId = walletId;
-        
+
         return await this.save(transaction);
     }
+
+
+
+    async getTransactionsOrderBy(walletId: string,where: FindConditions<TransactionEntity> , sort?: Sort, skip?: number, take?: number):
+        Promise<TransactionEntity[] | any> {
+        let transactionsWhereAndJoin = this.createQueryBuilder('transactions')
+            .innerJoinAndSelect('transactions.bank', 'bank');
+
+       
+        const transaction = await transactionsWhereAndJoin
+            .andWhere(where)
+            .skip(skip || 0)
+            .take(take || 0)
+            .getMany();
+        return transaction;
+    }
+    async countTransactionByWalletId(walletId: string): Promise<number> {
+        const count = await this.count(
+            {
+                where: {
+                    walletId: walletId
+                }
+            });
+        return count;
+    }
+
 }
